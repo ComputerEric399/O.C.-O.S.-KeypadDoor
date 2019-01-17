14236c088268e51c269eda4ae00e15a3499d5ff3
ComputerEric399 <42761313+ComputerEric399@users.noreply.github.com>
2019-01-16 21:28:41 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,23 @@
-while true do
-  computer.beep()
+for _, v in component.list("filesystem") do
+  if not component.proxy(v).exists("/notme") then
+    dait = v
+    break
+  end
+end
+for _, v in component.list("filesystem") do
+  if component.proxy(v).exists("/notme") then
+    daif = v
+    break
+  end
+end
+if daif ~= then
+  if dait ~= nil then
+    for file in daif.list() do
+      from = daif.open("/"..file)
+    end
+  else
+    error("No Other Drive Found")  
+  end
+else
+  error("Can't Install")
 end
