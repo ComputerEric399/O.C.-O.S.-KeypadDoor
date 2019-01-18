f447c26f32eaf93b6f82589b4f5e38850b325e7a
ComputerEric399 <42761313+ComputerEric399@users.noreply.github.com>
2019-01-17 17:27:08 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,31 +1,55 @@
+--find an other drive to install to
 for a, _ in component.list("filesystem") do
   if not component.proxy(a).exists("/notme") then
-    dcit = a
+    dcit = component.proxy(a)
+    computer.setBootAddress(a)
     break
   end
 end
+
+--find myself the floppy
 for a, _ in component.list("filesystem") do
   if component.proxy(a).exists("/notme") then
-    dcif = a
+    dcif = component.proxy(a)
     break
   end
 end
-if dcif ~= nil then
-  if dcit ~= nil then
-    for filename in dcif.list() do
-      filecopyfrom = dcif.open("/Files/"..filename)
-      filecopyto = dcit.open("/"..filename)
+
+--log all components
+log = dcif.open("/log.txt","w")
+for k, v in pairs(component.list()) do
+  label = ""
+  if v == "filesystem" then label = " || "..component.proxy(k).getLabel() end
+  dcif.write(log,k.."="..v..label.."\n")
+end
+dcif.close(log)
+
+--move all files from floppy to drive
+if dcit ~= nil then
+  if dcif ~= nil then
+    for _, filename in pairs(dcif.list("/Files/")) do
+      filecopyfrom = dcif.open("/Files/"..filename,"r")
+      data = ""
       while true do
         thing = dcif.read(filecopyfrom,1)
-        if thing == nil then break end
-        dcit.write(filecopyto,thing)
+        if thing == nil then
+          break
+        else
+          data = data..thing
+        end
       end
       dcif.close(filecopyfrom)
+      filecopyto = dcit.open("/"..filename,"w")
+      dcit.write(filecopyto,data)
       dcit.close(filecopyto)
     end
   else
-    error("No Other Drive Found")  
+    error()  
   end
 else
   error("Can't Install")
 end
+
+--restart
+computer.beep()
+computer.shutdown(true)
